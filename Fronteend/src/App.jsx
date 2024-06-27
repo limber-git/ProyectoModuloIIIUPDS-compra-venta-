e38b3c7ee2dd7fc7ce02028e9c85ebec21fff0ad
royernick <royeraso67@gmail.com>
2024-06-26 21:23:53 -0400

    primer envio del frontend

---
+++ Fronteend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Componente from './components/prueba';
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <Componente />
+    </div>
+  );
+};
+export default App
---
+++ Fronteend/src/components/prueba.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>hola</h1>
+      <p>Esto sera en capa de componentes</p>
+    </div>
+  );
+};
+
+export default MyComponent;
